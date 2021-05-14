54fcc0995f2341a2e80fdd852441a876594cd07b
Nam Dang <dangquangnam75@gmail.com>
2021-05-13 20:59:27 -0500

    second commit

--- ex_8.py
+++ ex_8.py
@@ -1,3 +1,13 @@
 # Write a Python program to extract year, month, date and time using Lambda.
 
-info = "2020-01-15 09:03:32.744178"
+import datetime
+
+time = datetime.datetime.now()
+print(time)
+
+year = lambda x: x.year
+month = lambda x: x.month
+day = lambda x: x.day
+print(f"the year is {year(time)}")
+print(f"the month is {month(time)}")
+print(f"the day is {day(time)}")
